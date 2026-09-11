9b4cee1d1fa512876a6181ada4b01a97f85bfadf
Felipex8000 <felipe.isern@gmail.com>
2026-09-11 20:55:36 -0300

    Datos simples (tambien simple)

---
+++ Tipos de Datos/datos-simples.py
@@ -0,0 +1,21 @@
+# Texto (string), se usan comillas "" comillas dobles """" o comillas triples """"""
+"string de una linea"
+""""string
+de
+varias
+lineas
+""""
+'string de una linea?'
+''''string
+de
+varias
+lineas?''''
+
+# enteros (int)
+40
+20
+30
+# flotantes (float)
+30.1
+30.2
+30.3
--- holamundo.py
+++
@@ -1,2 +0,0 @@
-print("Hola mundo")
-print(2 + 2)
