7acfd16bdb215e8c6a918963a918c17daa165f6f
Felipex8000 <felipe.isern@gmail.com>
2026-09-14 20:23:00 -0300

    operadores logicos

---
+++ operadores/op_logicos.py
@@ -0,0 +1,22 @@
+#And
+
+and1 = True & True
+and2 = False & True
+and3 = True & False
+and4 = False & False
+
+#Or
+
+or1 = True | True
+or2 =True | False
+or3 =False| True
+or4 =False| False
+
+#Not
+
+not1 = not True
+not2 = not False
+
+not3 = True & ("juan" == "JUAN")| (2 == 3)
+
+print (not3)
