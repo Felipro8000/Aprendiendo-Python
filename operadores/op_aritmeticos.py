04312055bb6f9492ec7e480444cc0993fd82169a
Felipex8000 <felipe.isern@gmail.com>
2026-09-14 10:00:04 -0300

    Operadores aritmeticos

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,20 @@
+#suma y resta (+ y -)
+suma = 15 + 2
+resta = 15 - 2
+
+#multiplicación y división (* y /)
+mult = 15 * 2
+div = 15 / 5 #devuelve dato flotante
+
+# potenciación (exxponente) **
+exponente = 15 ** 2
+
+#división baja (//)
+division_baja = 15 // 2 #devuelve entero redondeado para abajo
+
+#resto
+resto = 15 % 2
+
+#type (dato) devuelve el tipo de dato de la variable
+tipo_de_dato = type (resto)
+
