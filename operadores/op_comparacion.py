abeac0dc7992870adbec4eff21d434c1bccb143a
Felipex8000 <felipe.isern@gmail.com>
2026-09-14 10:47:04 -0300

    operadores de comparacion

---
+++ operadores/op_comparacion.py
@@ -0,0 +1,32 @@
+#Los op de comparación devuelven siempre True o False
+
+#un solo "=" asigna valor, pero "==" compara valores
+
+igual_de = 5 == 6
+
+distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual_que = 5 >= 6
+
+menor_o_igual_que = 5 <= 6
+
+#se explican bastante solos
+
+#calculos combinados 
+
+a = 22
+b = 12
+c = 34
+comp = a + b == c
+
+#comparar strs
+nombre = "Juan"
+nombre2 = "Pedro"
+comp2 = nombre == nombre2
+
+print (comp2)
+print (comp)
