9c267975e6ed9b0c2c619088ce67405f5c5375d6
Felipex8000 <felipe.isern@gmail.com>
2026-09-15 00:06:06 -0300

    inputs

---
+++ inputs/inputs.py
@@ -0,0 +1,18 @@
+#Como pedirle un dato de tipo str al usuario
+
+nombre = input("Pone tu nombre acá: ")
+
+#Nombre = nombre*2
+
+print (f"Tu nombre es: {nombre}")
+
+#sin importar que ingrese el usuario se va a tratar siempre como un str (probar poner nombre 11) y descomentar la multiplicación
+#"6" * 2 = "66" pero 6 * 2 = 12
+# Texto       vs      Num
+
+#Como pedir un numero
+numero = float(input("Elejir un numero:"))
+
+numero = int(numero * 2)
+
+print (f"El numero es: {numero}")
